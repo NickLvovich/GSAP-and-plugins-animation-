1f243b63dccdd9a1223a2d17cff287917996d42a
nik <kol-3cl@outlook.com>
2020-01-31 17:29:52 +0200

    photo animation

--- src/App.js
+++ src/App.js
@@ -1,64 +1,39 @@
 import React, {useRef, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-import {TweenMax, Power3} from 'gsap'
+import './App.scss';
+import photo from './assets/photo.jpg';
+import CSSRulePlugin from "gsap/CSSRulePlugin"
+import {TimelineLite, Power2} from 'gsap'
 
 
 function App() {
-  let logoItem = useRef(null)
-  let textItem = useRef(null)
-  let linkItem = useRef(null)
-  useEffect(() => {
-    TweenMax.to(
-      logoItem,
-      6,
-      {
-        opacity:1,
-        y:-20,
-        ease: Power3.easeOut
-      }
-    )
-    TweenMax.to(
-      textItem,
-      6,
-      {
-        opacity:1,
-        y:-20,
-        ease: Power3.easeOut
-      }
-    )
-    TweenMax.to(
-      linkItem,
-      3,
-      {
-        opacity:1,
-        y:-20,
-        ease: Power3.easeOut,
-        delay: 1
-      }
-    )
+  let container = useRef(null)
+  let image = useRef(null)
+  let imageReveal = CSSRulePlugin.getRule('.img-container:after')
+
+  const tl = new TimelineLite()
+  
+  useEffect( ()=>{
+    tl
+    .to(container, 1, {css: {visibility: 'visible'}})
+    .to(imageReveal, 1, {width: '0%', ease:Power2.easeOut })
+    .from(image, 0.8, {scale: 2, ease:Power2.easeInOut, delay: 0.3})
   }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img 
-        ref={el=>{logoItem=el}}
-        src={logo} className="App-logo" alt="logo" />
-        <p ref={el=>{textItem=el}}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a 
-          ref={el=>{linkItem=el}}
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="main">
+      <div 
+      ref={el=>(container=el)}
+      className="container">
+        <>
+          <div className="img-container">
+            <img 
+            ref={el=>(image=el)}
+            src={photo} />
+          </div>
+        </>
+      </div>
+    </section>
+    
   );
 }
 
