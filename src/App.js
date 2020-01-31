95afeb1b3e29486006201b6e4f2cea59cc0b98e8
nik <kol-3cl@outlook.com>
2020-01-31 15:50:13 +0200

    animation with circle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,57 +6,48 @@ import {TweenMax, Power3} from 'gsap'
 
 
 function App() {
-  let logoItem = useRef(null)
-  let textItem = useRef(null)
-  let linkItem = useRef(null)
-  useEffect(() => {
-    TweenMax.to(
-      logoItem,
-      6,
-      {
-        opacity:1,
-        y:-20,
-        ease: Power3.easeOut
-      }
-    )
-    TweenMax.to(
-      textItem,
-      6,
-      {
-        opacity:1,
-        y:-20,
-        ease: Power3.easeOut
-      }
-    )
-    TweenMax.to(
-      linkItem,
-      3,
-      {
-        opacity:1,
-        y:-20,
-        ease: Power3.easeOut,
-        delay: 1
-      }
-    )
+  let app = useRef(null)
+  let circleYellow = useRef(null)
+  let circleMaroon = useRef(null)
+  let circleBlueviolet = useRef(null)
+
+  const [circleState, setState] = useState(false)
+
+  const handleExpand = () => {
+    TweenMax.to(circleYellow, .8, {width: 200, height: 200, ease: Power3.easeOut})
+    setState(true)
+  }
+  const handleShrink = () => {
+    TweenMax.to(circleYellow, .8, {width: 75, height: 75, ease: Power3.easeOut})
+    setState(false)
+  }
+
+  useEffect( () => {
+    TweenMax.to( app, 0, {css: {visibility:'visible'}} )
+    // TweenMax.from(circleYellow, .8, {opacity:0, x: 40, ease: Power3.easeOut, 
+    // delay: 0.5})
+    // TweenMax.from(circleMaroon, .8, {opacity:0, x: 40, ease: Power3.easeOut, delay: 1})
+    // TweenMax.from(circleBlueviolet, .8, {opacity:0, x: 40, ease: Power3.easeOut, delay: 1.5})
+    TweenMax.staggerFrom([circleYellow, circleMaroon, circleBlueviolet], .8,{opacity:0, x: 40, ease: Power3.easeOut}, 0.5)
   }, [])
+
   return (
-    <div className="App">
+    <div
+    ref={ el => app = el}  
+    className="App">
       <header className="App-header">
-        <img 
-        ref={el=>{logoItem=el}}
-        src={logo} className="App-logo" alt="logo" />
-        <p ref={el=>{textItem=el}}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a 
-          ref={el=>{linkItem=el}}
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="circle-container">
+          <div
+          onClick={circleState !== true ? handleExpand : handleShrink}
+          ref={ el => circleYellow = el}
+          className="circle yellow"></div>
+          <div
+          ref={ el => circleMaroon = el}
+          className="circle maroon"></div>
+          <div
+          ref={ el => circleBlueviolet = el}
+          className="circle blueviolet"></div>
+        </div>
       </header>
     </div>
   );
